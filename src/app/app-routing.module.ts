ca06b3c07a197269a498f4713d850c345c755868
Assist Consultores <dfmesa@assist.com.co>
2019-07-21 13:07:17 -0500

    Componentes y templates iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ViewListComponent } from './components/view-list/view-list.component';
+import { CreateItemComponent } from './components/create-item/create-item.component';
+import { EditItemComponent } from './components/edit-item/edit-item.component';
+import { DeleteItemComponent } from './components/delete-item/delete-item.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'items', component: ViewListComponent },
+  { path: 'crear-item', component: CreateItemComponent },
+  { path: 'editar-item', component: EditItemComponent },
+  { path: 'eliminar-item', component: DeleteItemComponent },
+  { path: '**', component: NotFoundPageComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ViewListComponent } from './components/view-list/view-list.component';
+import { CreateItemComponent } from './components/create-item/create-item.component';
+import { EditItemComponent } from './components/edit-item/edit-item.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { DeleteItemComponent } from './components/delete-item/delete-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    ViewListComponent,
+    CreateItemComponent,
+    EditItemComponent,
+    NotFoundPageComponent,
+    DeleteItemComponent
   ],
   imports: [
     BrowserModule,
